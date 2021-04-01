3857fdcd7cc076340d93ccb7a56ea327e34aaee6
Tuzilow <1176281967@qq.com>
2021-04-01 09:14:48 +0800

    example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+    <v-app-bar app flat>
+      <div>教研活动中心</div>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container class="main-content" fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+    <v-footer absolute app>
+      版权信息
+    </v-footer>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  overflow-y: hidden;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+.main-content {
+  padding-left: 3rem !important;
+  padding-right: 3rem !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import message from './plugins/message';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(message);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
