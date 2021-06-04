c0afd53ea5b6fddbd0b54c58f31f91001a40a61d
xmy6364 <mengyu6498@icloud.com>
2021-06-04 16:22:32 +0800

    Update ActiveCalendar.vue

--- src/views/front/components/ActiveCalendar.vue
+++ src/views/front/components/ActiveCalendar.vue
@@ -1,7 +1,12 @@
 <template>
   <v-card class="active-calendar">
     <v-card-title>活动日历</v-card-title>
-    <v-calendar short-months locale="zh-CN" :day-format="emptyFormat" :month-format="emptyFormat">
+    <v-calendar
+      short-months
+      locale="zh-CN"
+      :day-format="emptyFormat"
+      :month-format="emptyFormat"
+    >
       <template slot="day" slot-scope="info">
         <v-tooltip left v-if="haveEvent(info)">
           <template v-slot:activator="{ on, attrs }">
@@ -14,14 +19,22 @@
               v-bind="attrs"
               v-on="on"
               @mouseover="getEventByTime(info)"
-            >{{ info.day }}</div>
+            >
+              {{ info.day }}
+            </div>
           </template>
           <v-list style="background: transparent;">
             <v-list-item color="transparent" v-if="showLoading">
               <span class="white--text">加载中...</span>
             </v-list-item>
-            <v-list-item v-else v-for="(event, index) in showEvents" :key="index">
-              <li class="white--text">{{ event.title }} - {{ event.room_name }}</li>
+            <v-list-item
+              v-else
+              v-for="(event, index) in showEvents"
+              :key="index"
+            >
+              <li class="white--text">
+                {{ event.title }} - {{ event.room_name }}
+              </li>
             </v-list-item>
           </v-list>
         </v-tooltip>
@@ -32,7 +45,9 @@
             info.outside ? 'grey--text darken-1' : 'black--text',
             haveEvent(info) ? 'has-event' : '',
           ]"
-        >{{ info.day }}</div>
+        >
+          {{ info.day }}
+        </div>
       </template>
     </v-calendar>
   </v-card>
@@ -65,11 +80,8 @@ export default {
         const currentDate = `${cur.year}-${cur.month}-${cur.day}`;
 
         if (
-          dayjs(currentDate).isBetween(
-            event.start_time,
-            event.end_time,
-            null,
-            '[]'
+          dayjs(currentDate).isSame(
+            dayjs(dayjs(event.start_time).format('YYYY-MM-DD'))
           )
         ) {
           return true;
@@ -80,7 +92,10 @@ export default {
       const [
         { data: doneActives },
         { data: willDoActives },
-      ] = await fetchActiveListBothDoingAndWillDo({ current_page: 1, page_size: 6 });
+      ] = await fetchActiveListBothDoingAndWillDo({
+        current_page: 1,
+        page_size: 6,
+      });
       this.events = [...doneActives.act, ...willDoActives.act];
     },
     async getEventByTime(cur) {
@@ -99,6 +114,9 @@ export default {
 
 <style lang="scss">
 .active-calendar {
+  .theme--light.v-btn.v-btn--has-bg {
+    background-color: #979595b4;
+  }
   .custom-day {
     position: absolute;
     top: 0;
@@ -117,7 +135,7 @@ export default {
     cursor: pointer;
   }
   .custom-day.has-event::after {
-    content: "";
+    content: '';
     display: block;
     background-color: #186fad;
     width: 7px;
